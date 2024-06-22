e8c5deb666f8ac75959601ac7dde3d3c3cfd4acb
Weiyuan Wu <weiyuan@crows.land>
2024-06-21 20:15:54 -0700

    update

--- src/__main__.py
+++ src/__main__.py
@@ -21,32 +21,17 @@ def main():
 @click.option("-d", "--day", required=False, type=int, default="1")
 @click.option("--detail", required=False, type=bool, default=False)
 def opt(day: int, detail: bool):
-    # foods = [
-    #     (Food.SALT, 1.88, 1.88),
-    #     (Food.CHICKEN_BREAST, 82, 82),
-    #     (Food.BROCCOLI, 20, 39),
-    #     (Food.CARROT, 20, 28),
-    #     (Food.SWEET_POTATO, 100, 238),
-    #     (Food.CANOLA_OIL, 11, 11),
-    #     # (Food.SOYBEAN, 0, 100),
-    #     (Food.BALANCEIT, 0, 5.62),
-    # ]
     foods = [
-        (Food.SALT, 0, 1.88),
-        (Food.CHICKEN_BREAST, 0, 200),
-        (Food.PORK_LIVER, 0, 100),
-        (Food.BROCCOLI, 0, 500),
-        (Food.CARROT, 0, 500),
+        (Food.SALT, 0, 2),
+        (Food.BEEF, 0, 50),
+        (Food.PORK_LIVER, 0, 50),
+        (Food.BROCCOLI, 0, 50),
+        (Food.EGG, 0, 50),
+        (Food.CARROT, 0, 50),
+        (Food.CUCUMBER, 0, 50),
         (Food.SWEET_POTATO, 0, 300),
-        (Food.CANOLA_OIL, 0, 11),
-        (Food.CHINESE_LETTUS, 0, 200),
-        (Food.BANANA, 0, 700),
-        (Food.CELERY, 0, 700),
-        (Food.EGG, 0, 300),
-        (Food.SOY_MILK, 0, 500),
+        (Food.CANOLA_OIL, 0, 10),
         (Food.BARF, 0, 4),
-        # (Food.SOYBEAN, 0, 100),
-        # (Food.BALANCEIT, 0, 5.62),
     ]
     needs = dog(age=2, weight=7, active=False)
 
